4eb27ab66192b83cd8df6f369a7e3d5480f418e6
Shenho Parallux <mqnjjj@gmail.com>
2025-06-10 22:39:29 +0300

    Сделал бургер)))))))

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -2,5 +2,14 @@ import LogoImage from "./header/Logo.svg";
 import BurgerImage from "./burger/Burger.svg";
 import BannerBackgroundImage from "./banner/background-banner.jpg";
 import ButtonIcon from "./callback-icon/icon.svg";
+import LocateIcon from "./burger/locate.svg";
+import PhoneIcon from "./burger/phone.svg";
 
-export { LogoImage, BurgerImage, BannerBackgroundImage, ButtonIcon };
+export {
+  LogoImage,
+  BurgerImage,
+  BannerBackgroundImage,
+  ButtonIcon,
+  LocateIcon,
+  PhoneIcon,
+};
--- src/shared/ui/burger/BurgerButton.tsx
+++ src/shared/ui/burger/BurgerButton.tsx
@@ -1,20 +1,105 @@
-import { BurgerImage } from "@/shared/assets/images";
 import "./BurgerButton.scss";
+import { LocateIcon, PhoneIcon } from "@/shared/assets/images";
 
 export const BurgerButton = () => {
+  const handleMenuLinkClick = () => {
+    const checkbox = document.getElementById(
+      "burger-toggle",
+    ) as HTMLInputElement;
+
+    if (checkbox) {
+      checkbox.checked = false;
+    }
+  };
+
   return (
-    <button
-      type="button"
-      className={`header__navigation-burger visible-mobile`}
-    >
-      <img
-        className="header__navigation-burger__image"
-        src={BurgerImage}
-        alt="Burger menu image"
-        width="40"
-        height="40"
-        loading="lazy"
+    <div className="burger visible-mobile">
+      <input
+        id="burger-toggle"
+        className="burger__button-toggle"
+        type="checkbox"
       />
-    </button>
+      <label htmlFor="burger-toggle" className="burger__button">
+        <span className="burger__button-line--top"></span>
+        <span className="burger__button-line--middle"></span>
+        <span className="burger__button-line--bottom"></span>
+      </label>
+
+      <div className="burger__menu">
+        <nav className="burger__menu-navigation">
+          <ul className="burger__menu-navigation__list">
+            <li className="burger__menu-navigation__item">
+              <a
+                href="#programs"
+                className="burger__menu-navigation__link"
+                onClick={handleMenuLinkClick}
+              >
+                Образовательные программы
+              </a>
+            </li>
+            <li className="burger__menu-navigation__item">
+              <a
+                href="#teachers"
+                className="burger__menu-navigation__link"
+                onClick={handleMenuLinkClick}
+              >
+                Наши преподаватели
+              </a>
+            </li>
+            <li className="burger__menu-navigation__item">
+              <a
+                href="#for"
+                className="burger__menu-navigation__link"
+                onClick={handleMenuLinkClick}
+              >
+                Для кого обучение
+              </a>
+            </li>
+            <li className="burger__menu-navigation__item">
+              <a
+                href="#graduates"
+                className="burger__menu-navigation__link"
+                onClick={handleMenuLinkClick}
+              >
+                Наши выпускники
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="burger__menu-access">
+          <a href="tel:8(862)254-47-43" className="burger__menu-access__number">
+            <img
+              className="burger__menu-access__number-icon"
+              src={PhoneIcon}
+              alt=""
+              width="40"
+              height="40"
+              loading="lazy"
+            />
+            <span className="burger__menu-access__number-main">
+              8 (862) 254-47-43
+            </span>
+          </a>
+          <a
+            href="https://yandex.eu/maps/-/CHSW6ZZ8"
+            className="burger__menu-access__address"
+            target={"_blank"}
+          >
+            <img
+              className="burger__menu-access__address-icon"
+              src={LocateIcon}
+              alt=""
+              width="40"
+              height="40"
+              loading="lazy"
+            />
+            <span className="burger__menu-access__address-main">
+              г. Сочи, ул. Чайковского, 45
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
