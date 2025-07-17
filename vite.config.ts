58ffb6f0642b964a5f312c2200470d7cba68f2b0
Shenho Parallux <mqnjjj@gmail.com>
2025-07-17 23:29:38 +0300

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/LandingPage-college/",
+  base: "/Mastery-of-taste-UETK/",
   css: {
     preprocessorOptions: {
       scss: {
